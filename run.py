02b9edda5e9e192c8d90e9775f166d7d42e56e8e
/home/fullpwn <57821130+fullpwn@users.noreply.github.com>
2022-05-30 12:18:52 +1000

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+from waybackpy import WaybackMachineSaveAPI
+url = "https://fullpwn.is-a.dev"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
+
+save_api = WaybackMachineSaveAPI(url, user_agent)
+save_api.save()
