b2c97a10ba47d85b850a3d2df578793b045e05ae
/home/fullpwn <57821130+fullpwn@users.noreply.github.com>
2022-05-30 14:33:59 +1000

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ main = "https://fullpwn.is-a.dev"
 whatsnew = "https://fullpwn.is-a.dev/whats-new/"
 new = "https://fullpwn.is-a.dev/new"
 preview = "https://fullpwn.is-a.dev/preview/"
+archive = "https://fullpwn.is-a.dev/archive/"
+archive1 = "https://fullpwn.is-a.dev/archive/22/05/"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
 
 save_api = WaybackMachineSaveAPI(main, user_agent)
@@ -17,3 +19,9 @@ print("Saved new")
 save_api = WaybackMachineSaveAPI(preview, user_agent)
 print(save_api.save())
 print("Saved Preview")
+save_api = WaybackMachineSaveAPI(archive, user_agent)
+print(save_api.save())
+print("Saved Archive")
+save_api = WaybackMachineSaveAPI(archive1, user_agent)
+print(save_api.save())
+print("Saved Archive 05/22")
